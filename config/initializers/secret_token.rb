d323601c491dcadf2be2b64a0ae8b2f2eee93ea0
Olivier Lacan <olivier.lacan@gmail.com>
2010-12-21 21:45:07 -0500

    Created Rails 3 project and added a few core gems: compass, html5-boilerplate, haml, haml-rails, devise, formtastic, jquery-rails, hpricot, ruby_parser.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweetsuiteAuth::Application.config.secret_token = 'b75e177772a7c01c8661d244608b4fb16900d30abcb9333f9bed32f5267579ac159ada1b99bac30b738fa1238d07fc70b3e37089a517e33915c89faa94932f09'
